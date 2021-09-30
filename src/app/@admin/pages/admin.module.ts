02321edf51074087b706fde8cc2da6d5e92aeede
Martin <majok5040@gmail.com>
2021-09-30 18:00:59 -0500

    03 - Public template
    Empty path. Absolute path of the public and admin section. Core public components.

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { TitleComponent } from '../core/components/title/title.component';
-import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
-import { NavbarComponent } from '../core/components/navbar/navbar.component';
+
+import { TitleComponent } from '@admin-core/components/title/title.component';
+import { SidebarComponent } from '@admin-core/components/sidebar/sidebar.component';
+import { NavbarComponent } from '@admin-core/components/navbar/navbar.component';
 
 
 @NgModule({
--- src/app/@public/pages/public.module.ts
+++ src/app/@public/pages/public.module.ts
@@ -4,10 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 
+import { NavbarComponent } from '@public-core/components/navbar/navbar.component';
+import { FooterComponent } from '@public-core/components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
